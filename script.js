4001f68aa79fd2284f91c71de504ca7b2f0071d5
Макс Лик <maxlikarenko@gmail.com>
2023-10-11 14:30:26 +0200

    - поле input в дисплее заменено на div - исправлены баги со сменой знака

--- script.js
+++ script.js
@@ -4,37 +4,39 @@ function appendToResult(value) {
     if (['/', '*', '-', '+'].includes(value) && currentInput === '')
         currentInput = 0
     currentInput += value;
-    document.getElementById('result').value = currentInput;
+    document.getElementById('result').innerHTML = currentInput;
 }
 
 function calculateResult() {
     try {
         currentInput = eval(currentInput).toString();
-        document.getElementById('result').value = currentInput;
+        document.getElementById('result').innerHTML = currentInput;
     } catch (error) {
-        document.getElementById('result').value = 'Error';
+        document.getElementById('result').innerHTML = 'Error';
     }
 }
 
 function clearResult() {
     currentInput = '';
-    document.getElementById('result').value = '0';
+    document.getElementById('result').innerHTML = '0';
 }
 
 function clearLast() {
     currentInput = currentInput.slice(0, -1)
-    document.getElementById('result').value = currentInput;
+    document.getElementById('result').innerHTML = currentInput;
 }
 
 
 function changeSign() {
-    if (currentInput === '')
-        currentInput = "-0"
+    if (currentInput === '') {
+        currentInput = "-"
+        document.getElementById('result').innerHTML = '-0'
+    }
     else {
         if (currentInput[0] === '-')
             currentInput = currentInput.substring(1)
         else
             currentInput = `-${currentInput}`
+        document.getElementById('result').innerHTML = currentInput;
     }
-    document.getElementById('result').value = currentInput;
 }
