7bec67adc084a70d8ef6f240aed2d67b4ee7ee48
Макс Лик <maxlikarenko@gmail.com>
2023-10-11 14:24:14 +0200

    - добавлена возможность коректной смены знака

--- script.js
+++ script.js
@@ -25,3 +25,16 @@ function clearLast() {
     currentInput = currentInput.slice(0, -1)
     document.getElementById('result').value = currentInput;
 }
+
+
+function changeSign() {
+    if (currentInput === '')
+        currentInput = "-0"
+    else {
+        if (currentInput[0] === '-')
+            currentInput = currentInput.substring(1)
+        else
+            currentInput = `-${currentInput}`
+    }
+    document.getElementById('result').value = currentInput;
+}
