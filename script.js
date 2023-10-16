d1ba395576cfc9294dae94de7fab50a5f531bb8b
Your Name <you@example.com>
2023-10-16 18:06:08 +0200

    ArKi_T

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function appendToResult(value) {
     // если к пустой строке добавить знак, то перед знаком допишется 0
     if (['/', '*', '-', '+'].includes(value) && currentInput === '')
         currentInput = 0
+<<<<<<< HEAD
 
     // если поставить точку к пустой строке или сразу после знака, то перед точкой поставится 0
     if (value === '.' && (currentInput === '' || ['/', '*', '-', '+'].includes(currentInput[currentInput.length - 1])))
@@ -33,6 +34,17 @@ function appendToResult(value) {
     if (value !== '.' && currentInput.slice(currentInput.search(/[+\-*/][0-9]$/) + 1) === '0')
         currentInput = currentInput.slice(0, -1);
 
+=======
+    if (value === '.' && (currentInput === '' || ['/', '*', '-', '+'].includes(currentInput[currentInput.length - 1]))) {
+        currentInput += '0'
+    }
+    if (['/', '*', '-', '+'].includes(value) && ['/', '*', '-', '+'].includes(currentInput[currentInput.length - 1]))
+        currentInput = currentInput.slice(0, -1);
+    if (['/', '*', '-', '+'].includes(value) && currentInput[currentInput.length - 1] === '.')
+        return;
+    if (value === '.' && currentInput.indexOf('.') !== -1)
+        return;
+>>>>>>> main
     currentInput += value;
     showLine();
 }
